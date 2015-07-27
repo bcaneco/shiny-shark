66d8d50e3500b025be6a2bff1f3847de33ac5e15
bcaneco <bruno@dmpstats.com>
2015-07-27 13:10:24 +0100

    BC: commit before Carl mess it up

--- server.R
+++ server.R
@@ -3,6 +3,15 @@ library(shiny)
 
 shinyServer(function(input, output) {
 
+  # render a plot for the catches
+  output$catchPlot <- renderPlot({
+    x <- seq(1,3)
+    y <- c(input$shll_mu, input$deep_mu, input$shkln_mu)
+    par(mar = c(5, 4, 0, 1))
+    plot(x, y, main = "")
+  })
+  
+  
 #   output$distPlot <- renderPlot({
 # 
 #     # generate bins based on input$bins from ui.R
--- ui.R
+++ ui.R
@@ -3,10 +3,10 @@ library(shiny)
 
 # Define input widget for lognormal distribution
 logN.Input <- function(title, prefix, mu.value, sd.value){
-  column(4, wellPanel(
+  column(2, wellPanel(
     h4(title),
-    sliderInput(paste0(prefix, "_mu"), "Expected Value:", min = 0, max = 1, mu.value),
-    sliderInput(paste0(prefix, "_sd"), "Standard deviation:", min = 0, max = 1, sd.value)
+    sliderInput(paste0(prefix, "_mu"), "Expected Value:", min = 0, max = 1, step = 0.2, mu.value),
+    sliderInput(paste0(prefix, "_sd"), "Standard deviation:", min = 0, max = 1, step = 0.2, sd.value)
   ))
 }
 
@@ -19,7 +19,7 @@ shinyUI(
              tabPanel("Input Distributions",
                       
                       tags$style(type="text/css",
-                                 "label {font-size: 11px;}",
+                                 "label {font-size: 12px;}",
                                  ".recalculating {opacity: 1.0;}"
                       ),
                       
@@ -27,7 +27,11 @@ shinyUI(
                         h3("Catch model"),
                         logN.Input("Shallow Hooks", "shll", mu.value = 0.001, sd.value = 0.06),
                         logN.Input("Deep Hooks", "deep", mu.value = 0.7, sd.value = 0.06),
-                        logN.Input("Shark-line Hooks", "shkln", mu.value = 0.1, sd.value = 0.6)
+                        logN.Input("Shark-line Hooks", "shkln", mu.value = 0.1, sd.value = 0.6),
+                        
+                        column(6,
+                               plotOutput("catchPlot"))
+                        
                         ),
                       
                       fluidRow(
